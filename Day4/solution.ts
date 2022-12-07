ae8098fe78348b9036315dac8183b7b4e365f082
Evdokia Mina <evdokia.mina@matillion.com>
2022-12-07 20:59:11 +0000

    clean up

--- Day4/solution.ts
+++ Day4/solution.ts
@@ -2,7 +2,9 @@ import { readFile } from "./readTextFile";
 
 const data = readFile("data.txt");
 
-let total = 0;
+let totalAll = 0;
+let totalFiltered = 0;
+
 data.forEach((item) => {
   var ranges = item.split(",");
   const range1start = +ranges[0].split("-")[0];
@@ -19,25 +21,26 @@ data.forEach((item) => {
     range2Arr.push(index);
   }
 
-  // This is the solution for Part 1
-  // const containsAll2 = range1Arr.every((element) => {
-  //   return range2Arr.includes(element);
-  // });
-  // const containsAll1 = range2Arr.every((element) => {
-  //   return range1Arr.includes(element);
-  // });
+  // PART - s1
+  const containsAll2 = range1Arr.every((element) => {
+    return range2Arr.includes(element);
+  });
+  const containsAll1 = range2Arr.every((element) => {
+    return range1Arr.includes(element);
+  });
 
-  // if (containsAll2 || containsAll1) {
-  //   total++;
-  // }
+  if (containsAll2 || containsAll1) {
+    totalAll++;
+  }
 
-  // This is the solution for Part 2
+  // PART - 2
   const filteredArray = range1Arr.filter(function (n) {
     return range2Arr.indexOf(n) !== -1;
   });
   if (filteredArray.length > 0) {
-    total++;
+    totalFiltered++;
   }
 });
 
-console.log(total);
+console.log(totalAll); //511
+console.log(totalFiltered); //821
